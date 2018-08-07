810dce7f00661b2a608a382e67d7969de226b78d
MimLim <854688358@qq.com>
2018-08-07 19:55:51 +0800

    添加实体

---
+++ entrance/src/main/java/com/marlee/myblog/service/UserService.java
@@ -0,0 +1,7 @@
+package com.marlee.myblog.service;
+
+import com.marlee.myblog.entity.User;
+
+public interface UserService {
+    int regist(User user);
+}
---
+++ entrance/src/main/java/com/marlee/myblog/service/impl/UserServiceImpl.java
@@ -0,0 +1,15 @@
+package com.marlee.myblog.service.impl;
+
+import com.marlee.myblog.entity.User;
+import com.marlee.myblog.service.UserService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserServiceImpl implements UserService{
+
+
+    @Override
+    public int regist(User user) {
+        return 0;
+    }
+}
